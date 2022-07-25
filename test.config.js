ac42363b03b261652da8f47c5de6917f83fc566b
jcote <jordocote@gmail.com>
2022-07-25 18:25:30 -0400

    cred

--- test.config.js
+++ test.config.js
@@ -6,7 +6,7 @@ module.exports = {
         watch: true,
         autorestart : false,
         env: {
-          "GOOGLE_APPLICATION_CREDENTIALS":"/home/chrx/.google/logos-162301-5257396ab743.json",
+          "GOOGLE_APPLICATION_CREDENTIALS":"/home/jordocote/.google/logos-162301-5257396ab743.json",
           "GCLOUD_PROJECT": "logos-162301",
           "CLOUD_BUCKET": "logos-162301",
           "MYSQL_USER":"logos",
