633a125bddd1d845f1be792bfb432c17d83ef9f8
jcote <jordocote@gmail.com>
2022-07-25 18:24:34 -0400

    promisify and fix interact

--- src/interact.js
+++ src/interact.js
@@ -3,49 +3,112 @@ const Sql = require('./sql');
 const FunctionParser = require('./functionparser.js');
 const F = require('./function');
 
-const interact = () => {
-      // get input from user prompt, output highest association
-        DataLib.readFreeIdentifierByName("readlineInputLine", (inputFreeIdentifier) => {
-            DataLib.readFreeIdentifierByName("readlineOutputLine", (outputFreeIdentifier) => {
+// get input from user prompt, output highest association
+async function interact () {
 
-                var storedInputFunction = FunctionParser.loadStoredFunction(inputFreeIdentifier);
-                var storedOutputFunction = FunctionParser.loadStoredFunction(outputFreeIdentifier);
+     // get readline code from database (not hard coded i/o here, rely on sensei)
+     async function getFreeIdentifierByName (name) {
+        return new Promise((resolve, reject) => {
+            DataLib.readFreeIdentifierByName(name, (freeIdentifier) => {
+                if (freeIdentifier == null) {
+                    return reject("couldn't retrieve "+name+" from database");
+                }
+                return resolve(freeIdentifier);
+            });
+        });
+     }
 
-                FunctionParser.parseFunction(storedInputFunction, null, (errIn) => {
-                    FunctionParser.parseFunction(storedOutputFunction, ["TESTING OUTPUT"], (errOut) => {
-                        if (errIn != null || errOut != null) {
-                            console.log(errIn + ", " + errOut);
-                            return setTimeout(interact, 1);
-                        }
-                        FunctionParser.executeFunction(storedInputFunction, null, (inputResultPromise) => {
-        
-                            inputResultPromise.then((inputResult) => {
+     // use above code to read from database and convert to DAO objects
+    const inputFreeIdentifier = await getFreeIdentifierByName("readlineInputLine")
+        .catch((reason) => {console.error(reason); return null;});
+    if (inputFreeIdentifier == null) {
+        return setTimeout(interact, 0);
+    }
+    const storedInputFunction = FunctionParser.loadStoredFunction(inputFreeIdentifier);
 
-                                DataLib.readFreeIdentifierByFn('"' + inputResult + '"', async (namedFreeIdentifier) => {
-                                    if (namedFreeIdentifier == null) {
-                                        console.log("*** C3 *** -> " + inputResult + " not found");
-                                        return setTimeout(interact, 1);
-                                    }
-                                    console.log(inputResult + " id: " + namedFreeIdentifier.id);
-                                    var randomAssociationId = await Sql.getRandomECAstId(namedFreeIdentifier.id);
-                                    if (randomAssociationId == null) {
-                                        console.log("*** C3 *** -> " + inputResult + " no assv found");
-                                        return setTimeout(interact, 1);
-                                    }
-                                    DataLib.readById(randomAssociationId, function(randomAssociation) {
-                                        console.log("****************************");
-                                        console.log(JSON.stringify(randomAssociation,null,4));
-                                        FunctionParser.executeFunction(storedOutputFunction, "OUTPUT:"+randomAssociation, () => {
-                                            return setTimeout(interact, 1);
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
+    const outputFreeIdentifier = await getFreeIdentifierByName("readlineOutputLine")
+        .catch((reason) => {console.error(reason); return null});
+    if (outputFreeIdentifier == null) {
+        return setTimeout(interact, 0);
+    }
+    const storedOutputFunction = FunctionParser.loadStoredFunction(outputFreeIdentifier);
+
+    // vv not required below
+/*
+    // fail-safe check the loaded function DAOs
+    async function parseIoFunctions () {
+        return new Promise((resolve, reject) => {
+            FunctionParser.parseFunction(storedInputFunction, null, (errIn) => {
+                if (errIn != null) {
+                    return reject(errIn);
+                }
+                FunctionParser.parseFunction(storedOutputFunction, ["TESTING OUTPUT"], (errOut) => {
+                    if (errOut != null) {
+                        return reject(errOut);
+                    }
+                    resolve();
                 });
             });
         });
-};
+    }
+
+    await parseIoFunctions();
+*/
+    // ^^ not required above
+
+
+    // get the input from prompt
+    async function getFreeIdentifierByInput() {
+        return new Promise((resolve, reject) => {
+            FunctionParser.executeFunction(storedInputFunction, null, async (inputResult) => {
+                // find the matching entry in the database (like a wordnet word)
+                await DataLib.readFreeIdentifierByFn('"' + inputResult + '"', (namedFreeIdentifier) => {
+                    if (namedFreeIdentifier == null) {
+                        return reject(inputResult + " not found");
+                    }
+                    return resolve(namedFreeIdentifier);
+                });
+            });
+        });
+    }
+
+    // read from input prompt and lookup the matching free identifier by name
+    const namedFreeIdentifier = await getFreeIdentifierByInput()
+        .catch((reason) => {console.error(reason); return null});
+    if (namedFreeIdentifier == null) {
+        return setTimeout(interact, 0);
+    }
+    console.log(namedFreeIdentifier.fn + " id: " + namedFreeIdentifier.id);
+
+    // find a random entry (using custom distribution)
+    // by the free identifier's equivalence class (lookup eq class by astid first)
+    async function getRandomByEquivalenceClass(namedFreeIdentifier) {
+        return new Promise(async (resolve, reject) => {
+            const randomAssociationId = await Sql.getRandomECAstId(namedFreeIdentifier.id);
+            if (randomAssociationId == null) {
+                return reject(namedFreeIdentifier.fn + " no assv found");
+            }
+            await DataLib.readById(randomAssociationId, function (randomAssociation) {
+                return resolve(randomAssociation);
+            });
+        });
+    }
+
+    // use ec (sql) db to find a random-by-associative-value entry
+    const randomAssociation = await getRandomByEquivalenceClass(namedFreeIdentifier)
+        .catch((reason) => {console.error(reason); return null});
+    if (randomAssociation == null) {
+        return setTimeout(interact, 0);
+    }
+
+    // output the random associative entry
+    console.log("*************ASSOCIA***************");
+    console.log(JSON.stringify(randomAssociation,null,4));
+    await FunctionParser.executeFunction(storedOutputFunction, ["OUTPUT:"+JSON.stringify(randomAssociation,null,4)], () => {
+        return;
+    });
+
+    return setTimeout(interact, 0);
+}
 
 exports.interact = interact;
