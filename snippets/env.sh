0c4c7c9a2db7f612e631f91c51017fa8c56eb718
Jordan Cote <jordocote@gmail.com>
2023-07-18 21:36:26 +0000

    env snippet

---
+++ snippets/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+          export MYSQL_USER="logos"
+          export MYSQL_PASSWORD="<password>"
+          export MYSQL_DATABASE="logos"
+          export MYSQL_HOST="127.0.0.1"
+          export MYSQL_PORT="33060"
+	  export OPENAI_API_KEY="<key>"
